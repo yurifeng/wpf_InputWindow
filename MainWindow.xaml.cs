0ff18714bf2aed4860069726615e5a1067cc2f4e
yurifeng <yuri_feng@outlook.com>
2018-05-10 16:47:38 +0800

    submit by yurifeng

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,5 +45,45 @@ namespace input_wpf
             }
             
         }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            //vs自带的messagebox小窗口
+            //第一个参数:显示的文本
+            //第二个参数:窗口标题
+            //第三个窗口:窗口按钮(为枚举值,为OK,Cancel,OKCancel)
+            //如何看用户点击的是确定还是取消?  答案:show的对话框也有返回值
+            MessageBoxResult msr = MessageBox.Show("确定还是取消?", "提示", MessageBoxButton.OKCancel);
+
+            //判断用户点击的结果
+            if (msr == MessageBoxResult.OK)
+            {
+                MessageBox.Show("确定");
+            }
+            else if (msr == MessageBoxResult.Cancel)
+            {
+               MessageBox.Show("取消");
+            }
+
+
+            //弹出对话框询问用户是否执行删除操作
+            MessageBoxResult msr2 = MessageBox.Show("确定要删除?", "警告", MessageBoxButton.YesNoCancel);
+
+            //判断用户点击的结果
+            if (msr2==MessageBoxResult.Yes)
+            {
+                MessageBox.Show("删除");
+            }
+            else if (msr2==MessageBoxResult.No)
+            {
+                MessageBox.Show("不删除");
+            }
+            else if (msr2==MessageBoxResult.Cancel)
+            {
+                MessageBox.Show("取消选择");
+            }
+            
+
+        }
     }
 }
